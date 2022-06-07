3c67d1d04b8e1ca7937bb8baa5ae6f97793a25a1
Tobias Racedo <tobaracedo@gmail.com>
2022-06-07 17:21:25 -0300

    I can get the location

---
+++ components/LocationFinder.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0"
+  >
+    <button ref="location-button" @click="getLocation()">
+      Get User Location
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LocationFinder',
+  methods: {
+    getLocation () {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+          console.log(position.coords)
+        })
+      } else {
+        alert('not supported')
+      }
+    }
+  }
+}
+</script>
