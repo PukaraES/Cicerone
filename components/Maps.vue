05c1313363a7f66c0589c5c0d07d7b0eede8234b
facuzero <facuzero_95@hotmail.com>
2022-06-08 00:32:52 -0300

    Componente maps al 30%

---
+++ components/Maps.vue
@@ -0,0 +1,7 @@
+<template>
+    <div> 
+        <iframe 
+            width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=-3.9688110351562504%2C40.25647271628502%2C-3.442840576171875%2C40.55450450121615&amp;layer=mapnik&amp;marker=40.40565356250165%2C-3.7058258056640625" style="border: 1px solid black">
+        </iframe>
+    </div>
+</template>
