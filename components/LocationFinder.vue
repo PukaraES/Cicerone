65bc6350743e75943262db245960b4710c67d94e
Tobias Racedo <tobaracedo@gmail.com>
2022-06-10 11:29:10 -0300

    Static search done

--- components/LocationFinder.vue
+++ components/LocationFinder.vue
@@ -9,8 +9,40 @@
 </template>
 
 <script>
+const wbk = require('wikibase-sdk')({
+  instance: 'https:/wikidata.org',
+  sparqlEndpoint: 'https://query.wikidata.org/sparql'
+})
+
+const sparql = `SELECT ?MUNICIPIO ?MUNICIPIOLabel ?MUNICIPIODescription ?coordenadas ?imagen WHERE {
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "es". }
+  ?MUNICIPIO wdt:P131 wd:Q1157977.
+  OPTIONAL { ?MUNICIPIO wdt:P625 ?coordenadas. }
+  OPTIONAL { ?MUNICIPIO wdt:P18 ?imagen. }
+}
+`
+const url = wbk.sparqlQuery(sparql)
+
 export default {
   name: 'LocationFinder',
+  data () {
+    return {
+      what: []
+    }
+  },
+  async fetch () {
+    this.what = await fetch(
+      url,
+      {
+        method: 'GET',
+        headers: ({
+          'Api-User-Agent': 'Just trying'
+        })
+      }
+    ).then(res => res.json())
+      .then(data => console.log(data.results.bindings[1].MUNICIPIOLabel.value))
+  },
+
   methods: {
     getLocation () {
       if (navigator.geolocation) {
