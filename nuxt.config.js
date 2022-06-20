ebb304e25214220353626e26f4d2b676467b7883
facuzero <facuzero_95@hotmail.com>
2022-06-20 09:54:38 -0300

    fix syntaxis eslint

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,10 +43,8 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-    // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa',
+    '@nuxtjs/axios', // https://go.nuxtjs.dev/axios
+    '@nuxtjs/pwa', // https://go.nuxtjs.dev/pwa
     'nuxt-leaflet'
   ],
 
