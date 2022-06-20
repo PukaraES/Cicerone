ab1005c00f6422b1d12a4b60c94cec064db8f69d
facuzero <facuzero_95@hotmail.com>
2022-06-20 10:42:54 -0300

    fix syntaxis eslint

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,7 @@ export default {
     '@nuxtjs/axios',
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
-    'nuxt-leaflet'    
+    'nuxt-leaflet'
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
