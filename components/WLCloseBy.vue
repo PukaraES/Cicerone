cd9774b082095d18999d2fb1a228e60a66a406d8
Orlando Pernia <angelus078@gmail.com>
2022-06-24 19:06:30 -0500

    Added search features
    Added the search feature to show nearby places from the search text, and deleted the search button.

--- components/WLCloseBy.vue
+++ components/WLCloseBy.vue
@@ -5,16 +5,19 @@
         Cerca de ti
       </p>
 
-      <div class="flex flex-row-reverse md:w-3/6 my-2 md:my-0">
-        <button class="mx-2 my-2 p-2 md:p-4 rounded-lg shadow-md md:active:bg-teal-700 md:active:text-white transition">
-          <i class="fa-solid fa-magnifying-glass mr-2 text-teal-500" /> Buscar
-        </button>
+      <div class="flex flex-row-reverse w-full md:w-3/6 my-2 md:my-0 relative">
+        <input v-model="search" type="text" class="md:mx-2 mx-auto w-11/12 md:w-full my-2 p-2 md:p-4 rounded-lg shadow-md" placeholder="Tu ubicación" @keyup="fetch">
 
-        <input type="text" class="mx-2 md:w-4/6 my-2 p-2 md:p-4 rounded-lg shadow-md" placeholder="Tu ubicación">
+        <div v-if="results != []" class="absolute w-full top-20 h-48 overflow-y-scroll">
+          <div v-for="result, index in results" :key="'result-'+index" class="bg-white text-gray-700 text-xl px-2 py-1 hover:bg-gray-300" @click="sendEntity(index)">
+            <span class="font-bold">{{ results[index].display.label.value | capitalize }}</span> -
+            {{ results[index].display.description ? results[index].display.description.value : 'No hay descripción' | capitalize }}
+          </div>
+        </div>
       </div>
     </div>
 
-    <WLCloseByMiniatures />
+    <WLCloseByMiniatures ref="miniatures" :coords="coords" />
   </div>
 </template>
 
@@ -22,13 +25,51 @@
 
 export default {
   name: 'WLCloseBy',
+  filters: {
+    capitalize (word) {
+      return word.toUpperCase()
+    }
+  },
   data () {
     return {
-
+      search: '',
+      results: [],
+      entity: '',
+      coords: []
     }
   },
   methods: {
+    sendEntity (index) {
+      this.entity = this.results[index].id
+      this.sendCoord()
+      this.results = ''
 
+      setTimeout(() => {
+        this.$refs.miniatures.updateCoords()
+      }, 500)
+    },
+    async fetch () {
+      const url = `https://www.wikidata.org/w/api.php?action=wbsearchentities&origin=*&format=json&type=item&search=${this.search}&language=es&uselang=es`
+
+      await fetch(url)
+        .then(res => res.json())
+        .catch(rerr => rerr)
+        .then((data) => {
+          this.results = data.search
+        })
+        .catch(derr => derr)
+    },
+    async sendCoord () {
+      const url = `https://www.wikidata.org/w/api.php?action=wbgetclaims&format=json&entity=${this.entity}&property=p625&origin=*`
+
+      await fetch(url)
+        .then(res => res.json())
+        .catch(rerr => rerr)
+        .then((data) => {
+          this.coords = data.claims
+        })
+        .catch(derr => derr)
+    }
   }
 }
 </script>
